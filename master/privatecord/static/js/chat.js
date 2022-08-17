7014e06b9a3b8fd1a3e96a78f3245431e70800c7
quar15 <quar5@wp.pl>
2022-08-17 09:58:50 +0200

    add: basic chat styling

--- master/privatecord/static/js/chat.js
+++ master/privatecord/static/js/chat.js
@@ -1,3 +1,23 @@
+function createMsgHtml(data){
+    var msg = $('<div>').addClass('message');
+    
+    var avatar = $('<div>').addClass('avatar');
+    avatar.append($('<img>').attr('src', "/static/img/profile_default.png"));
+    msg.append(avatar);
+    
+    var contentBox = $('<div>').addClass('content-box');
+    var header = $('<div>').addClass('header');
+    header.append($('<h3>').text(data['username']));
+    header.append($('<p>').text(data['date']));
+    contentBox.append(header);
+    var content = $('<div>').addClass('content');
+    content.text(data['msg']);
+    contentBox.append(content);
+    msg.append(contentBox);
+
+    $('#messages').append(msg);
+}
+
 $(document).ready(()=>{
     var socket = io.connect('localhost:8080');
     var currRoom = 'General';
@@ -5,17 +25,27 @@ $(document).ready(()=>{
     socket.on('connect', ()=>{});
 
     socket.on('message', (data)=>{
-        $('#messages').append($('<p>').text(data));
+        createMsgHtml(data);
+        $("#messages, .content-box").scrollTop($("#messages, .content-box")[0].scrollHeight);
     });
 
     $('#sendBtn').on('click', ()=>{
-        socket.send($('#username').val() + ': ' + $('#msg').val(), currRoom);
+        socket.send({'username': $('#username').val(), 'msg': $('#msg').val()}, currRoom);
         $('#msg').val('');
+        $("#messages, .content-box").scrollTop($("#messages, .content-box")[0].scrollHeight);
     });
 
     $('#joinBtn').on('click', ()=>{
         socket.emit('join', {'username': $('#username').val(), 'room': 'room-1'});
         currRoom = 'room-1';
+    });
+
+    $('#msg').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            console.log('Test');
+            $('#sendBtn').click();
+        }
     });
 });
 
